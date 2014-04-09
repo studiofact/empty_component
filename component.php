4837ad7624f3336af043717bc30d115b80df7779
livirex <livirex@gmail.com>
2014-04-09 12:29:04 +0600

    Добавление кеша к компоненту
    Если кеш действителен, метод отправляет на экран его содержимое, заполняет $arResult и возвращает False. Если кеш недействителен, метод возвращает True, кеширование завершается и кеш сохраняется при вызове методов

--- component.php
+++ component.php
@@ -1,4 +1,17 @@
 <?
-$arResult['TEXT'] = 'blabla';
-$this->IncludeComponentTemplate();
+
+if ($this->StartResultCache()) // http://dev.1c-bitrix.ru/api_help/main/reference/cbitrixcomponent/startresultcache.php
+{
+    // Запрос данных и заполнение $arResult
+    $arResult = "ID"
+
+    // Если выполнилось какое-то условие, то кешировать
+    // данные не надо
+    if ($arParams["ID"] < 10)
+        $this->AbortResultCache();
+
+    // Подключить шаблон вывода
+    $this->IncludeComponentTemplate();
+}
+
 ?>
